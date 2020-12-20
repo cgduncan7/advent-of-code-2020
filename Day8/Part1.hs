9ce4290abc7fcbbfba9f721e2288be6324d30965
cgduncan7 <cgduncan7@gmail.com>
2020-12-20 11:28:32 +0100

    day8 part1 done

--- Day8/Part1.hs
+++ Day8/Part1.hs
@@ -1,10 +1,10 @@
 import System.IO
 
 main = do
-  fileHandle <- openFile "data/short.txt" ReadMode
+  fileHandle <- openFile "data/input.txt" ReadMode
   fileContents <- hGetContents fileHandle
   let ls = lines fileContents
-  parseInstructions ls
+  print $ evaluateInstructions $ parseInstructions ls
   hClose fileHandle
 
 data Instruction = Ins { id :: Int
@@ -12,9 +12,34 @@ data Instruction = Ins { id :: Int
                        , arg :: Int
                        } deriving (Show)
 
+getInstructionId :: Instruction -> Int
+getInstructionId (Ins id _ _) = id
+
+getInstructionOp :: Instruction -> String
+getInstructionOp (Ins _ op _) = op
+
+getInstructionArg :: Instruction -> Int
+getInstructionArg (Ins _ _ arg) = arg
+
+data ProgramState = PS { pc :: Int
+                       , acc :: Int
+                       , instructions :: [Int]
+                       , halt :: Bool
+                       } deriving (Show)
+
+getProgramStatePC :: ProgramState -> Int
+getProgramStatePC (PS pc _ _ _) = pc
+
+getProgramStateAcc :: ProgramState -> Int
+getProgramStateAcc (PS _ acc _ _) = acc
+
 parseInstructions :: [String] -> [Instruction]
 parseInstructions [] = []
-parseInstructions s = foldr (\acc x -> parseInstruction acc x) 0 s
+parseInstructions (s:ss) = parseInstruction 0 s : parseInstructions' 1 ss
+
+parseInstructions' :: Int -> [String] -> [Instruction]
+parseInstructions' _ [] = []
+parseInstructions' i (s:ss) = parseInstruction i s : parseInstructions' (i+1) ss
 
 parseInstruction :: Int -> String -> Instruction
 parseInstruction _ "" = error "Empty string"
@@ -22,3 +47,17 @@ parseInstruction i s =
   let (o,a) = span (/= ' ') s
       b = read $ dropWhile (`elem` [' ', '+']) a :: Int
   in Ins i o b
+
+evaluateInstructions :: [Instruction] -> ProgramState
+evaluateInstructions [] = PS 0 0 [] False
+evaluateInstructions is = evaluateInstructions' (PS 0 0 [] False) is
+
+evaluateInstructions' :: ProgramState -> [Instruction] -> ProgramState
+evaluateInstructions' p [] = p
+evaluateInstructions' (PS a b c True) _ = PS a b c True
+evaluateInstructions' p i = evaluateInstructions' (evaluateInstruction p (i !! getProgramStatePC p)) i
+
+evaluateInstruction :: ProgramState -> Instruction -> ProgramState
+evaluateInstruction (PS pc acc is h) (Ins id op arg) = if id `elem` is
+  then PS id acc is True
+  else PS (pc + if op == "jmp" then arg else 1) (acc + if op == "acc" then arg else 0) (is ++ [id]) False
